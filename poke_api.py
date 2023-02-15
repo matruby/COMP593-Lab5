55aacbba5ce3792b5d9448519a7e49e939ca707f
matruby <Matthew.Rubie@flemingcollege.ca>
2023-02-14 21:41:33 -0500

    File completed
    Satisfied file requirements

--- poke_api.py
+++ poke_api.py
@@ -1,12 +1,40 @@
 
-def fetch_poke_info(pokename="", pokedex_num="")
+import pprint
+import requests as req
+import sys
+
+
+def fetch_poke_info(pokename="", pokedex_num=""):
     """
-    Get pokemon information and return a dictionary of Pokemon infomration
+    Retrieves Pokemon Information
+
+    :param pokename: Name of Pokemon
+    :param pokedex_num: Num of Pokemon in Pokedex
+    :returns: Dictionary of Pokemon Info
     """
+    # See if the Pokemon name was passed or the Pokedex number
+    if pokename:
+        print(f"Getting {pokename.title()}'s Information!!\n")
+        poke_info = req.get(f'https://pokeapi.co/api/v2/pokemon/{str(pokename)}')
+        # Check if the reponse succeded
+        if poke_info.status_code == req.codes.ok:
+            print(f'{pokename.title()} Found!\n'
+            poke_dict = poke_info.json()
+        else:
+            print(f"{pokename.title()} Doesn't Exist!\nRetry with a valid Pokemon!")
+            return None
     
-    if poke_dict:
-        return poke_dict
-    else:
-        return None
+    if pokedex_num:
+        print(f"Getting {pokename.title()}'s Information!!\n")
+        poke_info = req.get(f'https://pokeapi.co/api/v2/pokemon/{str(pokedex_num)}')
+        # Check if the reponse succeded
+        if poke_info.status_code == req.codes.ok:
+            print(f'{pokename.title()} Found!\n')
+            poke_dict = poke_info.json()
+        else:
+            print(f"{pokename.title()} Doesn't Exist!\nRetry with a valid Pokemon!")
+            return None
+ 
+    return poke_dict
 
 
